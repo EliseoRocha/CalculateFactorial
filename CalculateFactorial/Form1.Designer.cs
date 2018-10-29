c474ccecbf3dcc2b7385bbc29849c3957fba5d80
Eliseo Rocha <rochaeliseo91@gmail.com>
2018-10-29 15:54:35 -0700

    Initial commit

---
+++ CalculateFactorial/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CalculateFactorial
+{
+    partial class FactorialCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumberLabel = new System.Windows.Forms.Label();
+            this.NumberTextBox = new System.Windows.Forms.TextBox();
+            this.FactorialLabel = new System.Windows.Forms.Label();
+            this.FactorialTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NumberLabel
+            // 
+            this.NumberLabel.AutoSize = true;
+            this.NumberLabel.Location = new System.Drawing.Point(71, 66);
+            this.NumberLabel.Name = "NumberLabel";
+            this.NumberLabel.Size = new System.Drawing.Size(44, 13);
+            this.NumberLabel.TabIndex = 0;
+            this.NumberLabel.Text = "Number";
+            // 
+            // NumberTextBox
+            // 
+            this.NumberTextBox.Location = new System.Drawing.Point(177, 66);
+            this.NumberTextBox.Name = "NumberTextBox";
+            this.NumberTextBox.Size = new System.Drawing.Size(34, 20);
+            this.NumberTextBox.TabIndex = 1;
+            // 
+            // FactorialLabel
+            // 
+            this.FactorialLabel.AutoSize = true;
+            this.FactorialLabel.Location = new System.Drawing.Point(74, 129);
+            this.FactorialLabel.Name = "FactorialLabel";
+            this.FactorialLabel.Size = new System.Drawing.Size(47, 13);
+            this.FactorialLabel.TabIndex = 2;
+            this.FactorialLabel.Text = "Factorial";
+            // 
+            // FactorialTextBox
+            // 
+            this.FactorialTextBox.Location = new System.Drawing.Point(177, 129);
+            this.FactorialTextBox.Name = "FactorialTextBox";
+            this.FactorialTextBox.ReadOnly = true;
+            this.FactorialTextBox.Size = new System.Drawing.Size(271, 20);
+            this.FactorialTextBox.TabIndex = 3;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(74, 205);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateButton.TabIndex = 4;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(201, 205);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // FactorialCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 253);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.FactorialTextBox);
+            this.Controls.Add(this.FactorialLabel);
+            this.Controls.Add(this.NumberTextBox);
+            this.Controls.Add(this.NumberLabel);
+            this.Name = "FactorialCalculator";
+            this.Text = "Factorial Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NumberLabel;
+        private System.Windows.Forms.TextBox NumberTextBox;
+        private System.Windows.Forms.Label FactorialLabel;
+        private System.Windows.Forms.TextBox FactorialTextBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ CalculateFactorial/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateFactorial
+{
+    public partial class FactorialCalculator : Form
+    {
+        public FactorialCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            long number = Convert.ToInt64(NumberTextBox.Text);
+            long factorial = number;
+
+            if (IsLong(NumberTextBox.Text))
+            {
+                const byte minNum = 1;
+                const byte maxNum = 20;
+
+
+            }
+
+            for (int i = 1; i < number; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            FactorialTextBox.Text = factorial.ToString("N0");
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
